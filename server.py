8c1836afd684ca5e08d8e7b9b1b3167ffbe016d7
oksmartypants <cep2141@columbia.edu>
2016-03-28 00:05:53 -0400

    search for user

--- server.py
+++ server.py
@@ -1384,7 +1384,10 @@ def us():
 	fin=[]
 	if res:
 		user=""
+		print 'res'
+		print res
 		for thing in res:
+			print thing
 			user=thing[2]
 			stmt= "SELECT * from Friend f where f.uid1=%s and f.uid2=%s UNION SELECT * from FRIEND f where f.uid1=%s and f.uid2=%s"
 			cursor=g.conn.execute(stmt, (uid, user, user, uid))
