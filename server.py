e1f902422d41a06aec2bc0c6d2bfb40136c1455e
oksmartypants <cep2141@columbia.edu>
2016-03-29 00:26:17 -0400

    fixing utags

--- server.py
+++ server.py
@@ -1465,7 +1465,7 @@ def frevs():
 	enames=[]
 	tagdict={}
 	stmt="SELECT e.eid, t.tag_id, t.tname FROM Event_Create_Where e, Tags t, Marked m where and e.eid=m.eid and t.tag_id=m.tag_id"
-	cursor=g.conn.execute(stmt)
+	cursor=g.conn.execute(stmt,)
 	for result in cursor:
 		if result[0] in enames:
 			l=len(pw)
