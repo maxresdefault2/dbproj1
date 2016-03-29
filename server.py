87a0718ca0518c961344084a01b9a911462d6964
oksmartypants <cep2141@columbia.edu>
2016-03-28 21:57:25 -0400

    fixing utags

--- server.py
+++ server.py
@@ -590,6 +590,7 @@ def hhome():
 	rendedit=False
 	er=None
 	eev=0
+	print hid
 	stmt = "SELECT e.ename, h.hname, l.city, l.zip, l.state, l.loc_name, e.edate, e.time, e.photo, e.eid FROM Event_Create_Where e, Host h, Marked m, Location l where e.lid=l.lid and e.uid=h.uid and e.eid=m.eid and e.uid = %s"
 	cursor = g.conn.execute(stmt, (hid,))
 	nt=[]
