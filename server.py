170533222ba3cd10929b6bc4443e554ff524f5cb
oksmartypants <cep2141@columbia.edu>
2016-03-29 22:45:57 -0400

    fix

--- server.py
+++ server.py
@@ -2243,7 +2243,7 @@ if __name__ == "__main__":
 
     HOST, PORT = host, port
     print "running on %s:%d" % (HOST, PORT)
-    app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 
   run()
